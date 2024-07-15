744228e86a850213000d85f0a897322adb2df5df
techiehe <huala.fun@outlook.com>
2024-07-15 23:16:29 +0800

    fix

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,13 +1,14 @@
 import { sans } from "@/lib/fonts";
 import "@/styles/markdown.css";
-
+import Image from "next/image";
 export default function NotFound() {
   return (
-    <article className="markdown">
+    <article className="markdown select-none flex justify-center flex-col items-center gap-4">
+      <Image src={"./404.svg"} alt="" width={400} height={400}/>
       <h1
         className={[
           sans.className,
-          "text-[40px] font-black leading-[44px] text-[--title]",
+          "text-[40px] font-black text-center leading-[44px] text-[--title]",
         ].join(" ")}>
         Not found
       </h1>
